a792cc0c2dbeaecffb73b904d59b1209f718b278
Aerol <aerolcagalingan@gmail.com>
2019-09-27 14:08:20 +0800

    TAB

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,31 +1,16 @@
 import { Component } from '@angular/core';
-import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
-import { NavController } from '@ionic/angular';
+
+import { Routes } from '@angular/router';
+
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
 
-  myPhoto:any;
+  constructor() {} 
 
-  constructor(public navCtrl: NavController, private camera:Camera) {}
-  
-  takePhoto(){
-    const options: CameraOptions = {
-      quality: 100,
-      destinationType: this.camera.DestinationType.FILE_URI,
-      encodingType: this.camera.EncodingType.JPEG,
-      mediaType: this.camera.MediaType.PICTURE
-    }
-    
-    this.camera.getPicture(options).then((imageData) => {
-    // imageData is either a base64 encoded string or a file URI
-    // If it's base64 (DATA_URL):
-    this.myPhoto = 'data:image/jpeg;base64,' + imageData;
-    }, (err) => {
-    // Handle error
-    });
-  }
 }
