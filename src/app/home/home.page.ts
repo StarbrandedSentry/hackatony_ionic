661d7d43a9063b4d28de5c646e12042531216e83
StarbrandedSentry <hierarchhero@gmail.com>
2019-09-27 13:40:25 +0800

    rwa
    rwarwa

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,6 @@
 import { Component } from '@angular/core';
-<<<<<<< HEAD
-import { Platform } from '@ionic/angular';
-import { Camera, CameraOptions, DestinationType } from '@ionic-native/camera/ngx';
-
-=======
 import { Camera, CameraOptions } from '@ionic-native/camera/ngx';
 import { NavController } from '@ionic/angular';
->>>>>>> 4d604b89b304527a47023223c699159f2cb9b55c
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -14,27 +8,6 @@ import { NavController } from '@ionic/angular';
 })
 export class HomePage {
 
-<<<<<<< HEAD
-  constructor(private camera: Camera, private platform: Platform) {}
-
-  options: CameraOptions = {
-    quality: 100,
-    destinationType: this.camera.DestinationType.FILE_URI,
-    encodingType: this.camera.EncodingType.JPEG,
-    mediaType: this.camera.MediaType.PICTURE
-  }
-
-  takePicture(){ 
-    if(this.platform.is('cordova')){
-      this.camera.getPicture(this.options).then((imageData) => {
-        let base64Image = 'data:image/jpeg;base64,' + imageData;
-      }, (err) => {
-        console.log('something went wrong with the camera');
-      });
-    }
-  }
-}
-=======
   myPhoto:any;
 
   constructor(public navCtrl: NavController, private camera:Camera) {}
@@ -56,4 +29,3 @@ export class HomePage {
     });
   }
 }
->>>>>>> 4d604b89b304527a47023223c699159f2cb9b55c
